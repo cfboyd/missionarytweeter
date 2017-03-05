9a201189e84f18b55d90b5704590edd9c0435b49
cfboyd <cfboyd@gmail.com>
2017-03-05 11:52:05 -0800

    whatever

--- appTwitReply.js
+++ appTwitReply.js
@@ -3,8 +3,7 @@ var responses =
 ['Missionary sex is underrated!',
 'Missionary position is loving and intimate',
 'Missionary is a classic for a reason',
-'dont talk shit about missionary position',
-'why are you hating on missionary sex? its actually very good',
+'missionary sex is actually very good',
 'missionary is cool. not all sex has to be freaky to be good',
 'missionary position is great for kissing your partner',
 'missionary position is very comfortable for both parties',
@@ -18,18 +17,22 @@ var T = new Twit({
   access_token_secret: 'hXOJMs4evEDKlKEKsVpAqWtu4ODqeskhiWV2QdnYvY6xU'
 })
 
+//i dont remmeber why i did this
 var tweeet = 'test';
 
+//tracking terms
 var stream = T.stream('statuses/filter', { track: ['missionary sex', 'missionary position'] })
 
+//replying to mentions of those terms
 stream.on('tweet', function (tweet) {
   console.log("@" + tweet.user.screen_name + " " + tweet.text)
 
-  function newPost(tweeet) {
-    var nameID = tweet.id_str;
+  function newPost(tweeet) {      //function for a new post
+    var nameID = tweet.id_str;    //the id for the tweet ya found
 
+    //the actual post
     T.post('statuses/update', {in_reply_to_status_id: nameID, status: tweeet }, function(err, data, response) {
-      if (err) {console.log(err);
+      if (err) {console.log(err); // logs an error if u get one
     }
       else {console.log(tweeet)}
   })
@@ -40,7 +43,7 @@ stream.on('tweet', function (tweet) {
     var reply = "@" + tweet.user.screen_name + " " + responses[Math.floor(Math.random() * responses.length)]
     newPost(reply);}
 
-//tweets at an interval, not as a reply
+//tweets from the array at an interval, not as a reply
 })
 setInterval(
   function newStatus() {
